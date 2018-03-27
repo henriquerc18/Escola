969c59d9b5ad6c3d9163a96c79fe23e531cd6914
Henrique Rosa Carvalho <henriquerc2015@gmail.com>
2018-03-26 21:17:19 -0300

    Migração Eclipse.

---
+++ src/escola/Aluno.java
@@ -0,0 +1,23 @@
+package escola;
+
+public class Aluno extends Pessoa {
+    private long matricula = 0;
+   
+    public long getMatricula(){
+        return matricula;
+    } 
+              
+    public void setMatricula(long matricula){
+        this.matricula = matricula;
+    }
+
+    public Aluno(String nome, String email, long matricula){
+        super(nome, email);
+        this.matricula = matricula;
+    }
+
+    public void mostraDados(){
+        super.mostraDados();
+        System.out.println("Matrícula: " + this.matricula);
+    }
+}
---
+++ src/escola/Professor.java
@@ -0,0 +1,35 @@
+package escola;
+
+public class Professor extends Pessoa {
+    private String disciplina = "";
+    private int cargaHoraria = 0;
+
+    public String getDisciplina(){
+        return disciplina;
+    } 
+              
+    public void setDiscipĺina(String disciplina){
+        this.disciplina = disciplina;
+    }
+     
+    public int getCargaHoraria(){
+        return cargaHoraria;
+    } 
+      
+    public void setCargaHoraria(int cargaHoraria){
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Professor(String nome, String email, String disciplina, int cargaHoraria){
+        super(nome, email);
+        this.disciplina = disciplina;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public void mostraDados(){
+        super.mostraDados();
+        System.out.println("Disciplina: " + this.disciplina);
+        System.out.println("Carga Horária: " + this.cargaHoraria); 
+    }
+}    
+        
